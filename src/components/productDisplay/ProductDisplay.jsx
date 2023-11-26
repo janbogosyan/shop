38b65420aad786f0592d2ecfa4fbdb480ff24fb0
Jan Bogosyan <janbogosyan@gmail.com>
2023-11-26 19:23:32 +0200

    adding logic and fixing

--- src/components/productDisplay/ProductDisplay.jsx
+++ src/components/productDisplay/ProductDisplay.jsx
@@ -15,7 +15,7 @@ const ProductDisplay = (props) => {
                     <img src={product.image} alt="product" />
                 </div>
                 <div className="productdisplay-img">
-                    <img src classNameproductdisplay-main-img={product.image} alt="product" />
+                    <img src={product.image} className='productdisplay-main-img' alt="product" />
                 </div>
             </div>
             <div className="productdisplay-right">
@@ -29,8 +29,25 @@ const ProductDisplay = (props) => {
                     <p>(122)</p>
                 </div>
                 <div className="productdisplay-right-prices">
-                    
+                    <div className="productdisplay-right-price-old">${product.old_price}</div>
+                    <div className="productdisplay-right-price-new">${product.new_price}</div>
                 </div>
+                <div className="productdisplay-right-description">
+                    A lightweight, usually knitted ,very good material and unique hype style
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-size">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XLL</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
+                <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
             </div>
         </div>
     )
